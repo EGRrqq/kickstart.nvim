c88b449b56ab9a329a53b6db7c7da9ad5b5c8b8a
egr <egrrqqdev@gmail.com>
2026-03-10 16:20:27 +0500

    jeez

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -4,35 +4,46 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  lazy = false,
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  ---@module 'neo-tree'
-  ---@type neotree.Config
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    version = '*',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+    },
+    lazy = false,
+    keys = {
+      { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+    },
+    ---@module 'neo-tree'
+    ---@type neotree.Config
+    opts = {
+      filesystem = {
+        hijack_netrw_behavior = 'disabled',
+        window = {
+          mappings = {
+            ['\\'] = 'close_window',
+          },
         },
-      },
-      filtered_items = {
-        visible = true,        -- Show hidden files
-        hide_dotfiles = false, -- Don't hide dotfiles
-        hide_gitignored = false,
-        hide_by_name = {
-          -- Add any additional files you want to hide explicitly
+        filtered_items = {
+          visible = true, -- Show hidden files
+          hide_dotfiles = false, -- Don't hide dotfiles
+          hide_gitignored = false,
+          hide_by_name = {
+            -- Add any additional files you want to hide explicitly
+          },
+          never_show = {},
         },
-        never_show = {},
       },
     },
+  },
+  {
+    'antosha417/nvim-lsp-file-operations',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-neo-tree/neo-tree.nvim', -- makes sure that this loads after Neo-tree.
+    },
+    config = function() require('lsp-file-operations').setup() end,
   },
 }
